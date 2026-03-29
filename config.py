f3f9f785d772238cbedbac349a03d001f51cc8b7
MSA <ent7be@gmail.com>
2026-03-29 19:00:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -74,4 +74,13 @@ SYMBOLS = [
     # 11-20 - Major Alts
     'LINK/USDT', 'DOT/USDT', 'BCH/USDT', 'NEAR/USDT', 'LTC/USDT',
     'UNI/USDT', 'ATOM/USDT', 'XLM/USDT', 'HBAR/USDT', 'ICP/USDT',
+    # 21-30 - Established Coins
+    'XTZ/USDT', 'ETC/USDT', 'FIL/USDT', 'VET/USDT', 'ALGO/USDT',
+    'MANA/USDT', 'SAND/USDT', 'AXS/USDT', 'AAVE/USDT', 'IOTA/USDT',
+    # 31-40 - Popular Coins
+    'NEO/USDT', 'THETA/USDT', 'DASH/USDT', 'GRT/USDT', 'RUNE/USDT',
+    'EGLD/USDT', 'CHZ/USDT', 'GALA/USDT', 'ENJ/USDT', 'ZIL/USDT',
+    # 41-50 - DeFi & Others
+    'COMP/USDT', 'SNX/USDT', 'SUSHI/USDT', 'YFI/USDT', 'CRV/USDT',
+    '1INCH/USDT', 'ZEC/USDT', 'QTUM/USDT', 'KSM/USDT', 'SHIB/USDT',
 ]
